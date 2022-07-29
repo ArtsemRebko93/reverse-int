3dadd70da50309b5703449c9e5fd66ef5c43cce5
Artsem Rebko <artsem.rebko93@gmail.com>
2022-07-29 23:18:26 +0300

    refactor: change index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const toStr = String(n);
+    let reverseStr = toStr.split("");
+    if (reverseStr[0] === "-") {
+        let delIndex = reverseStr.shift();
+    }
+    reverseStr = reverseStr.reverse().join("");
+    return Number(reverseStr);
+};
